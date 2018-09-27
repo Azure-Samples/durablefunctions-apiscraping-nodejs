9d3408933b668e8dca5e9a40206720936236ae8d
sdras <sarah.drasner@gmail.com>
2018-09-27 15:10:14 -0600

    add repo/desc to package json to remove warnings, small optimizations for saverepo function

--- FanOutFanInCrawler/SaveRepositories/index.js
+++ FanOutFanInCrawler/SaveRepositories/index.js
@@ -1,37 +1,38 @@
-const df = require("durable-functions");
-var storage = require('azure-storage');
+const df = require('durable-functions')
+var storage = require('azure-storage')
 
-module.exports = async function (context, input) {
-    // `input` here is retrieved from the Orchestrator function `callActivityAsync` input parameter
+module.exports = async (context, input) => {
+  // `input` here is retrieved from the Orchestrator function `callActivityAsync` input parameter
 
-    // create the table service for Blob Storage
-    var tableService = storage.createTableService(process.env['AzureWebJobsStorage']);
-    
-    // create the table if it doesn't exist already.
-    tableService.createTableIfNotExists('Repositories', function (error) {
-        if (error) {
-            console.error(error);
-        }
-        if (!error) {
-            // creates a batch of operation to be executed
-            var batch = new storage.TableBatch();
-            for (var i = 0; i < input.length; i++) {
-                var repository = input[i];
+  // create the table service for Blob Storage
+  var tableService = storage.createTableService(
+    process.env['AzureWebJobsStorage']
+  )
 
-                // Creates an operation to add the repository to Table Storage
-                batch.insertOrReplaceEntity({
-                    PartitionKey: {'_': 'Default'},
-                    RowKey: {'_': repository.id.toString()},
-                    OpenedIssues: {'_': repository.openedIssues},
-                    RepositoryName: {'_': repository.name}
-                });
-            }
-            // execute the batch of operations
-            tableService.executeBatch('Repositories', batch, function (error, result, response) {
-                if (error) {
-                    console.error(error);
-                }
-            });
-        }
-    });
-};
+  // create the table if it doesn't exist already.
+  tableService.createTableIfNotExists('Repositories', error => {
+    if (error) {
+      console.error(error)
+      return
+    }
+    // creates a batch of operation to be executed
+    var batch = new storage.TableBatch()
+    for (var i = 0; i < input.length; i++) {
+      var repository = input[i]
+
+      // Creates an operation to add the repository to Table Storage
+      batch.insertOrReplaceEntity({
+        PartitionKey: { _: 'Default' },
+        RowKey: { _: repository.id.toString() },
+        OpenedIssues: { _: repository.openedIssues },
+        RepositoryName: { _: repository.name }
+      })
+    }
+    // execute the batch of operations
+    tableService.executeBatch('Repositories', batch, error => {
+      if (error) {
+        console.error(error)
+      }
+    })
+  })
+}
