5288ce41a8fe4e510fdb3c6801d326bb8e1c654b
sdras <sarah.drasner@gmail.com>
2018-09-28 17:52:48 -0600

    update formatting

--- FanOutFanInCrawler/Orchestrator_HttpStart/index.js
+++ FanOutFanInCrawler/Orchestrator_HttpStart/index.js
@@ -1,14 +1,16 @@
-const uuid = require('uuid/v4');
+const uuid = require('uuid/v4')
 
-module.exports = function (context, req, starter) {
-    var id = uuid();
-    
-    // Function input comes from the request content.
-    context.bindings.starter = [{
-        FunctionName: "Orchestrator",
-        Input: "Nuget",
-        InstanceId: id
-    }];
-    
-    context.done(null);
-};
+module.exports = function(context, req, starter) {
+  var id = uuid()
+
+  // Function input comes from the request content.
+  context.bindings.starter = [
+    {
+      FunctionName: 'Orchestrator',
+      Input: 'Nuget',
+      InstanceId: id
+    }
+  ]
+
+  context.done(null)
+}
